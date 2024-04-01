73e562a4770b345c2011c8fc429ae15a73a3262a
CODY <110027137+chirag3084@users.noreply.github.com>
2024-04-01 16:36:07 +0530

    Add files via upload

---
+++ P7.java
@@ -0,0 +1,12 @@
+package prac;
+
+import java.util.Random;
+public class P7 {
+    public static void main( String args[] ) {
+        //int val = 3;
+        int a = 3456;
+        String str = "F0R";
+        String val = new String(str + a);
+        System.out.println(val);
+    }
+}
